bbdf79b8f4863ce0344ae8510f01b0929fac2165
MiILasha <krotkovartem2008@gmail.com>
2024-10-05 16:25:11 +0500

    modified:   image/discord.svg 	new file:   image/steam.svg 	new file:   image/vk.svg 	renamed:    index.html -> kinopoisk.html 	modified:   style.css 	modified:   top-gun.html 	new file:   video.js

---
+++ video.js
@@ -0,0 +1,8 @@
+// Plyr video
+
+// Change the second argument to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('video', {captions: {active: true}});
+
+// Expose player so it can be used from the console
+window.player = player;
