4812160e62c5cdd26f99e9f5753e243468782182
MiILasha <krotkovartem2008@gmail.com>
2024-12-07 16:36:40 +0500

    Version 2.0.1

---
+++ js/inner.js
@@ -0,0 +1,28 @@
+$("#flooter").prepend(`<section class="feedback">
+            <div class="container">
+                <div class="col-sm-12 flooter-text">
+                    <spam class="flooter-text-year">© 2023 — 2024, Kinopoisk</spam>
+                    <spam class="flooter-text-company">Проект компании <spam class="flooter-text-company-name">Artem?</spam></spam>
+                </div>
+            </div>
+        </section>
+
+        <section class="logo-image">
+            <div class="container">
+                <div class="col-sm-12" align="center">
+                    <a href="https://vk.com/id484095375" class="title-image">
+                        <img src="image/vk.svg" class="title-image" alt="nope" id="vk">
+                    </a>
+                    <a href="https://t.me/xxartyomxx" class="title-image">
+                        <img src="image/telegram.svg" class="title-image" alt="nope" id="telegram">
+                    </a>
+                    <a href="https://youtube.com/@supersadman2022?si=Tk2DqeLEnzfyADe0" class="title-image">
+                        <img src="image/youtube.svg" class="title-image" alt="nope" id="youtube">
+                    </a>
+                    <a href="https://steamcommunity.com/profiles/76561198970098920/" class="title-image">
+                        <img src="image/steam.svg" class="title-image" alt="nope" id="stean">
+                    </a>
+                </div>
+            </div>
+        </section>
+`);
